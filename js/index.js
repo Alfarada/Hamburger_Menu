2664f8b12a991dfccc6ec4e750598737d98646a0
Alfredo <alfaradayepez@gmail.com>
2021-02-24 17:54:09 -0500

    Agregando código JavaScript que oculta el menú al hacer click en el enlace seccion.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import hamburgerMenu, { d } from "./hamburger_menu.js";
 
 d.addEventListener("DOMContentLoaded", (e) => {
-  hamburgerMenu(".panel-btn", ".panel");
+  hamburgerMenu(".panel-btn", ".panel", ".menu a");
 });
